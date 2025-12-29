6c60aa3d21dfd499605e9ae67092d1d605e9df87
Luis Fernando Aruquipa Gamarra <Fercho_Sk_669@hotmail.com>
2025-12-29 14:55:48 -0400

    projects urls

--- src/mock/Projects.js
+++ src/mock/Projects.js
@@ -1,7 +1,7 @@
 import { images } from "@/utils/imgLoader";
 
 export const projectData = [
-    { id: crypto.randomUUID(), imgUrl: images.TASKY, webUrl: '', repository: 'https://github.com/jfher/Taskify', title: 'Taskify', description: 'Plataforma para gestion de tareas', stack: ['NodeJs', 'Express', 'JWT', 'MongoDB', 'Docker', 'React', 'Zustand', 'Tanstack Query'] },
-    { id: crypto.randomUUID(), imgUrl: images.HEPAWS, webUrl: '', repository: 'https://github.com/jfher/HealthyPaws', title: 'HealthyPaws', description: 'Plataforma para adopcion de mascotas para una veterinaria local', stack: ['NodeJs', 'Express', 'JWT', 'MongoDB', 'Docker', 'React', 'Zustand', ' TailwindCSS', 'ShadCN'] },
-    { id: crypto.randomUUID(), imgUrl: images.PLANIFY, webUrl: '', repository: 'https://github.com/jfher/PlanifyHub', title: 'PlanifyHub', description: 'Plataforma de gestion de vida, tareas, habitos y metas para mejorar dia a dia', stack: ['NodeJs', 'Express', 'JWT', 'MongoDB', 'Docker', 'React', 'Axios', 'Zustand', 'Zod', ' TailwindCSS', 'ShadCN'] },
+    { id: crypto.randomUUID(), imgUrl: images.TASKY, webUrl: 'https://jfher-taskify.netlify.app', repository: 'https://github.com/jfher/Taskify', title: 'Taskify', description: 'Plataforma para gestion de tareas', stack: ['NodeJs', 'Express', 'JWT', 'MongoDB', 'Docker', 'React', 'Zustand', 'Tanstack Query'] },
+    { id: crypto.randomUUID(), imgUrl: images.HEPAWS, webUrl: 'https://healthypawsjfhs.netlify.app', repository: 'https://github.com/jfher/HealthyPaws', title: 'HealthyPaws', description: 'Plataforma para adopcion de mascotas para una veterinaria local', stack: ['NodeJs', 'Express', 'JWT', 'MongoDB', 'Docker', 'React', 'Zustand', ' TailwindCSS', 'ShadCN'] },
+    { id: crypto.randomUUID(), imgUrl: images.PLANIFY, webUrl: 'https://planifyhub.netlify.app', repository: 'https://github.com/jfher/PlanifyHub', title: 'PlanifyHub', description: 'Plataforma de gestion de vida, tareas, habitos y metas para mejorar dia a dia', stack: ['NodeJs', 'Express', 'JWT', 'MongoDB', 'Docker', 'React', 'Axios', 'Zustand', 'Zod', ' TailwindCSS', 'ShadCN'] },
 ]
